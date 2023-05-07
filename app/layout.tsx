8aea96380836c3a769a93f440596ee78ff576d5f
Nakayama Daichi <d@alumni.tus.ac.jp>
2023-05-08 00:56:00 +0900

    Add navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Noto_Sans_JP } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import Link from 'next/link'
 
 // Preloading enabled by default so an error occurs without subsetting
 const notoSansJp = Noto_Sans_JP({ subsets: ['latin'] })
@@ -21,7 +22,9 @@ export default function RootLayout({
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
           <header className="bg-blue-600 dark:bg-blue-800 text-white py-6">
             <div className="container mx-auto px-4">
-              <h1 className="text-4xl font-bold">Next.js sandbox</h1>
+              <h1 className="text-4xl font-bold">
+                <Link href="/">Next.js sandbox</Link>
+              </h1>
             </div>
           </header>
           {children}
